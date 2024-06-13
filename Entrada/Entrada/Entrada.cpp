66a281de8e46110e58a37ead58be430c1d8883a6
Maria Luiza Ribas <luuiza.ribass@gmail.com>
2024-06-13 16:43:43 -0300

    ,
    ,

---
+++ Entrada/Entrada/Entrada.cpp
@@ -0,0 +1,75 @@
+//#include <iostream>
+
+
+//using namespace std;
+//
+//int NumeroMaior(int num1, int num2) {
+//	if (num1 > num2) {
+//		return num1;
+//	}
+//	else {
+//		return num2;
+//	}
+//}
+//
+//int main() {
+//	int num1, num2;
+//
+//	cout << "Digite o seu primeiro numero: ";
+//	cin >> num1;
+//
+//	cout << "Digite o seu segundo numero: ";
+//	cin >> num2;
+//
+//	int maior = NumeroMaior(num1, num2);
+//
+//	cout << "O maior numero sera: " << maior << endl;
+//
+//		return 0;
+//}
+
+
+
+//using namespace std;
+//
+//int main() {
+//	int quantidade;
+//
+//	cout << "Informe a quantidade de produtos do estoque: ";
+//		cin >> quantidade;
+//
+//		if (quantidade >= 100){
+//			cout << "Quantidade suficiente no estoque." << endl;
+//		}
+//		else if (quantidade >= 50) {
+//			cout << "Faca um novo pedido e reabasteça o estoque" << endl;
+//		}
+//		else {
+//			cout << "Faca um novo pedido e reabasteça o estoque" << endl;
+//		}
+//
+//		return 0;
+//}
+
+
+
+//using namespace std;
+//
+//int main() {
+//	int numero;
+//
+//	cout << "Informe o nnmero: ";
+//	cin >> numero;
+//
+//	if (numero > 0) {
+//		cout << "o numero " << numero << " e positivo." << endl;
+//	}
+//	else if (numero < 0) {
+//		cout << "o numero " << numero << " e negatico." << endl;
+//	} 
+//	else {
+//		cout << "o numero e igual a " << numero << endl;
+//	}
+//	
+//	return 0;
+//}
